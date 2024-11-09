21ab5f673214321383ff932ab95f5d5b109817c2
Muhammad Bilal Khan <160259391+Bilalkhan0026@users.noreply.github.com>
2024-11-09 19:39:06 +0500

    Add files via upload

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    
+    extends: 'next/core-web-vitals',
+    rules: {
+      // yahan per apni specific rules bhi add kar sakte hain
+    },
+    rules: {
+      'react/no-unescaped-entities': 'off',
+    },
+
+      };
+      
+  
